307de8fc14bcd1936a3a8f92c391fa067a03c2be
Dylan Kelley <dkelley989@west-mec.org>
2019-04-24 15:05:54 -0700

    Website Update
    Finished the calcCart function by creating an if statement that checks if the selected index of the sessionBox selection list is not equal to -1, and if its not it stores the text of the selected option in the session storage variable confSession and the value of the selected option in the confSessionCost session storage variable; otherwise stores an empty text string and the value 0 respectively. Also checks If the user has clicked the mediaCB check box, and if the user has, stores the values “yes” and 115 in the confPack and confPackcost session storage variables; otherwise store the values “no” and 0.

--- mas_reg2.js
+++ mas_reg2.js
@@ -20,3 +20,18 @@
 
 
 */
+
+/// Event listener that runs the writeSessionValues() variable when the page loads.
+window.addEventListener('load', writeSessionValues);
+
+// Func: Writes data values from session storage in to the registration summary form.
+function writeSessionValues() {
+      document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+      document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+      document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+      document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+      document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+      document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+      document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal");
+}
--- mas_register.js
+++ mas_register.js
@@ -77,19 +77,60 @@ function sessionTest() {
 // Func: Calculates the cost of the registration and saves data in session storage.
 function calcCart() {
       /// Stores the values of the group, email, phoneNumber, and banquetGuests fields in the session storage variables: confGroup, confMail, confPhone, and confBanquet.
-      var confGroup = document.getElementById("groupBox").value;
-
-      var confMail = document.getElementById("mailBox").value;
-
-      var confPhone = document.getElementById("phoneBox").value;
+      sessionStorage.setItem("confGroup", document.getElementById("groupBox").value);
+      sessionStorage.setItem("confPhone", document.getElementById("phoneBox").value);
+      sessionStorage.setItem("confMail", document.getElementById("mailBox").value);
+      sessionStorage.setItem("confBanquet", document.getElementById("banquetBox").value);
 
       var confBanquet = document.getElementById("banquetBox").value;
 
-      var confBanquetCost = confBanquet * 55;
+      /// Multiplies the value of the banquetGuests field by 55.
+      sessionStorage.setItem("confBanquetCost", confBanquet * 55);
 
+      // Var: Refers the sessionBox selection list.
+      var sessionBox = document.getElementById("sessionBox");
+
+      // If: Checks if the selected index of the sessionBox selection list is not equal to -1.
       if (sessionBox.selectedIndex !== -1) {
-            var confSession = sessionBox.selectedIndex;
+            /// Stores the text of the selected option in the session storage variable confSession and the value of the selected option in the confSessionCost sesson storage variable. Otherwise stores an empty text string and the value 0 respectively.
+            sessionStorage.setItem("confSession", "");
+            sessionStorage.setItem("confPackCost", 0);
+            var confPackCost = 0;
       } else {
+            sessionStorage.setItem("confSession", "");
+            sessionStorage.setItem("confPackCost", 115);
+            var confPackCost = 115;
+      }
 
+      // If: Checks if the user has clicked the mediaCB checkbox.
+      if (document.getElementById("mediaCB").checked === true) {
+
+            /// Stores the values “yes” and 115 in the confPack and confPackcost session storage variables. Otherwise store the values “no” and 0.
+            sessionStorage.setItem("confPack", "yes");
+            sessionStorage.setItem("confPackCost", 115);
+            var confPackCost = 115;
+      } else {
+            sessionStorage.setItem("confPack", "no");
+            sessionStorage.setItem("confPackCost", 0);
+            var confPackCost = 0;
+      }
+
+      /// Calculates the total regisstration cost.
+      sessionStorage.setItem("confTotal", parseFloat(confSessionCost) + parseFloat(confBanquetCost) + parseFloat(confPackCost));
+
+      /// Calls the writeSessionValues() function.
+      writeSessionValues();
+
+      // Func: Writes data values from session storage in to the registration summary form.
+      function writeSessionValues() {
+            /// Sets the text content of span elements with the IDs “regName”, “regGroup”, “regEmail”, “reg- Phone”, “regSession”, “regBanquet”, and “regPack”, to the values of the session storage vari- ables, confName, confGroup, confMail, confPhone, confSession, confBanquet, and confPack.
+            document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+            document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+            document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+            document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+            document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+            document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+            document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+            document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal");
       }
 }
